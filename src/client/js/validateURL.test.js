08049f8a233f122d861658d7209a383dcc996bc5
mrana007 <63938469+mrana007@users.noreply.github.com>
2020-05-17 10:20:43 -0700

    Add files via upload

--- src/client/js/validateURL.test.js
+++ src/client/js/validateURL.test.js
@@ -1,7 +1,15 @@
 import { validateURL } from "./validateURL.js";
 
 test("Validates the protocol and domain of a URL", () => {
+  //create messagePlaceholder element for testing purposes
+  document.body.innerHTML = '<div id="errorMessage"></div>';
+
+  const messagePlaceholder = document.getElementById("errorMessage");
+  expect(validateURL("")).toBe(false);
   expect(validateURL("https://google.com")).toBe(true);
   expect(validateURL("https://medium.com")).toBe(true);
   expect(validateURL("httpss://medium.com")).toBe(false);
+  expect(validateURL("https://googlecom")).toBe(false);
+  expect(validateURL("https:/medium.com")).toBe(false);
+  expect(validateURL("http://medium.co")).toBe(true);
 });
